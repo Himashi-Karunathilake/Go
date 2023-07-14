4b28e2737d482aebeafd211b6cbe3fa83acd79a8
Himashi Karunathilake <himashikarunathilake@gmail.com>
2023-07-14 19:48:13 +0530

    Variable handling code piece added.

--- Part-1/main.go
+++ Part-1/main.go
@@ -5,5 +5,17 @@ package main
 import "fmt"
 
 func main() {
+	// Print a single line
 	fmt.Println("Hello World!")
+
+	// Print an empty line
+	fmt.Println()
+
+	// Declare variables to store a string and a number
+	name := "Rachael"
+	age := 22
+
+	// Print the variables
+	fmt.Println("Name of Programmer: ", name)
+	fmt.Println("Age of Programmer: ", age)
 }
